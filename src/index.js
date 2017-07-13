e00da245f26e6d62a2070300a5aeeaa01e95486b
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-13 15:24:32 -0500

    Setup Graphiql endpoint

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const { graphqlExpress } = require('graphql-server-express');
+const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 
 const schema = require('./schema');
 
 const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: 'graphql'
+}))
 
 const port = 3000;
 app.listen(port, () => {
